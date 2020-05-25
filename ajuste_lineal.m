4c3ccb0fcbfff4dbbf9f6ce7033036b85f9f1dbc
Joaquin Stella <joaquinstella99@gmail.com>
2020-05-25 15:02:02 -0300

    Se agrega el calculo de ajuste lineal

---
+++ ajuste_lineal.m
@@ -0,0 +1,19 @@
+﻿function [c0,c1] = ajuste_lineal(x,y)
+sumx = 0;
+sumy = 0;
+sumxy = 0;
+sumx2 = 0;
+for i = 1:size(x)(1),
+    disp(x(i));
+    disp(y(i));
+    sumx = sumx + x(i);
+    sumy = sumy + y(i);
+    sumxy = sumxy + x(i)*y(i);
+    sumx2 = sumx2 + x(i)*x(i);
+endfor
+xm = sumx/size(x)(1);
+ym = sumy/size(x)(1);
+c1 = (size(x)(1)*sumxy - sumx*sumy)/(size(x)(1)*sumx2 - sumx*sumx);
+c0 = ym - c1*xm;
+end
+
