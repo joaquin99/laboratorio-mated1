d5c55d181d329acc7a8dbb125d5bbfbe6af0988c
Joaquin Stella <joaquinstella99@gmail.com>
2020-06-01 19:36:57 -0300

    Se agregan las leyendas, elegirDato.m y la capacidad de elegir pais y dato

---
+++ elegirDato.m
@@ -0,0 +1,9 @@
+﻿function [opcion] = elegirDato(n)
+if (n == 'I')
+  opcion = 2;
+elseif (n == 'M')
+  opcion = 3;
+elseif(n == 'R')
+  opcion = 4;
+endif
+endfunction
--- grafica_lineal.m
+++ grafica_lineal.m
@@ -1,16 +1,23 @@
 pkg load io;
+
 fecha = {'28/03', '30/03', '31/03', '03/04', '05/04', '07/04', '09/04', '17/04', '01/05', '03/05'};
 timeFormat='dd/mm';
 xdatenum=datenum(fecha,timeFormat);
 
-datos = xlsread('DatosArgentina.xlsx');
-[c0,c1] = ajuste_lineal(xdatenum,datos(:,2));
+lugar = 'Argentina';
+datoAMostrar = 'Muertos';
+
+datos = xlsread(['Datos',lugar,'.xlsx']);
+[c0,c1] = ajuste_lineal(xdatenum,datos(:,elegirDato(datoAMostrar(1))));
 
 hold on
 
-scatter(xdatenum,datos(:,2))
-plot(xdatenum,c1*xdatenum+c0);
+scatter(xdatenum,datos(:,elegirDato(datoAMostrar(1))),'DisplayName',[datoAMostrar,' en ',lugar])
+plot(xdatenum,c1*xdatenum+c0,'DisplayName','Tendencia');
 datetick('x',timeFormat,'keepticks')
 
+xlabel('Fecha');
+ylabel('Cantidad');
+legend('Location','southeast');
 hold off
 
