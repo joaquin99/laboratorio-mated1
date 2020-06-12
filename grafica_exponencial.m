db417b2db4fcc1be513563b84044c8a8eae0f31f
Joaquin Stella <joaquinstella99@gmail.com>
2020-06-12 01:44:47 -0300

    Cambio menor en la representacion de los dias de la exponencial

--- grafica_exponencial.m
+++ grafica_exponencial.m
@@ -1,7 +1,7 @@
 pkg load io;
 
 fecha = {'28/03', '30/03', '31/03', '03/04', '05/04', '07/04', '09/04', '17/04', '01/05', '03/05'};
-parseoDatos = [1;2;3;6;8;10;12;20;34;36];
+parseoDatos = [0;2;3;6;8;10;12;20;34;36];
 timeFormat='dd/mm';
 xdatenum=datenum(fecha,timeFormat);
 
