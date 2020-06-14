86e33895851556e10983163e7176d3badd0a881c
Joaquin Stella <joaquinstella99@gmail.com>
2020-06-13 22:25:16 -0300

    Se arregla un error al calcular el coeficiente alfa

--- grafico_derivada.m
+++ grafico_derivada.m
@@ -4,6 +4,7 @@ fecha = {'28/03', '30/03', '31/03', '03/04', '05/04', '07/04', '09/04', '17/04',
 timeFormat='dd/mm';
 xdatenum=datenum(fecha,timeFormat);
 
+%Se leen los datos de los muertos para encontrar su derivada
 lugar = 'Argentina';
 datoAMostrar = 'Muertos';
 datos = xlsread(['Datos',lugar,'.xlsx']);
@@ -11,18 +12,21 @@ datos = xlsread(['Datos',lugar,'.xlsx']);
 xparseo = [0;2;3;6;8;10;12;20;34;36];
 [c0,c1] = ajuste_lineal(xparseo,log(datos(:,elegirDato(datoAMostrar(1)))));
 
+%Calculo las derivadas en cada punto, con h=0.01
 derivadas = [deriv_centrada_exp(xparseo,0.01,c0,c1)];
-disp(derivadas);
 
+%Coeficiente alfa de muertes
 coef_muerte = derivadas;
 
-for i=1;size(derivadas)(1),
-  coef_muerte(1) = coef_muerte(1)/(exp(c0)*exp(xparseo(i)*c1));
-endfor
-disp(coef_muerte);
-media_coef = mean(coef_muerte);
-disp(media_coef);
+%Para calcular alfa se divide la derivada de muertes por los infectados
+datoAMostrar = 'Infectados';
 
+for i=1;size(derivadas)(1),
+  coef_muerte(1) = coef_muerte(1)/(datos(:,elegirDato(datoAMostrar(1)))(i));
+endfor
+
+%Se toma la media aritmetica de alfa
+media_coef = mean(coef_muerte);
 
 hold on
 
